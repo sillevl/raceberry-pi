d44af7d49dd68db14efd9fdde0287f8f01a6be9b
Sille Van Landschoot <info@sillevl.be>
2015-05-26 14:05:18 +0200

    removed chartjs library

--- js/app.js
+++ js/app.js
@@ -1,46 +1,9 @@
 $(document).foundation();
 
-var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
-var lineChartData = {
-	labels : ["January","February","March","April","May","June","July"],
-	//labels : [],
-	datasets : [
-		{
-			label: "My First dataset",
-			fillColor : "rgba(220,220,220,0)",
-			strokeColor : "rgba(220,220,220,1)",
-			pointColor : "rgba(220,220,220,1)",
-			pointStrokeColor : "#fff",
-			pointHighlightFill : "#fff",
-			pointHighlightStroke : "rgba(220,220,220,1)",
-			//data : []
-			data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-		},
-		{
-			label: "My Second dataset",
-			fillColor : "rgba(151,187,205,0)",
-			strokeColor : "rgba(151,187,205,1)",
-			pointColor : "rgba(151,187,205,1)",
-			pointStrokeColor : "#fff",
-			pointHighlightFill : "#fff",
-			pointHighlightStroke : "rgba(151,187,205,1)",
-			//data : []
-			data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-		}
-	]
-}
 
 
 $(document).ready(function(){
-	var ctx = document.getElementById("chart").getContext("2d");
-	window.myLine = new Chart(ctx).Line(lineChartData, {
-		responsive: true,
-		animation : false,
-		scaleOverride: true,
-		scaleStartValue: 0,
-		scaleSteps: 10,
-		scaleStepWidth: 10
-	});
+
 });
 
 function milliToString(milli){
@@ -83,3 +46,4 @@ angular.module("raceberry-pi", [])
 		window.myLine.removeData();
 	}, 500);
 });
+
