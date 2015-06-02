7b83b0c293698a983a3a9f8730efcfe9ed0e2c43
Sille Van Landschoot <info@sillevl.be>
2015-06-02 21:49:37 +0200

    added state to the timer

--- app/timer.js
+++ app/timer.js
@@ -3,16 +3,39 @@ var log = require('./lib/logger');
 Timer = function(){
 
 	var startTime;
+	var status = 'waiting';
+	var time = 0;
 
 	this.start = function(){
 		log.info('Timer started');
 		startTime = (new Date).getTime();
+		status = 'running';
+	}
+
+	getTime = function(){
+		if(status == 'running'){
+			return (new Date).getTime() - startTime;
+		} else {
+			return time;
+		}
 	}
 
 	this.stop = function(){
 		var endTime = (new Date).getTime() - startTime;
 		log.info('Timer stopped at %dms', endTime);
+		status = 'stopped';
+		time = endTime;
 		return endTime;
+	}
+
+	this.reset = function(){
+		log.info('Timer reset');
+		time = 0;
+		status = 'waiting';
+	}
+
+	this.getStatus= function(){
+		return { status: status, time: getTime()};
 	}
 }
 
