44281e4c3860b679d6431a7ba5b046f3878001c1
Sille Van Landschoot <info@sillevl.be>
2015-06-03 10:35:20 +0200

    step1 adding detector

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ var Timer = require('./app/timer');
 var settings = require('./app/settings');
 
 
-timer = Timer.create();
+/**
+ *  RACEBERRY-PI 
+ */
+
 
 /**
  * Detector
@@ -16,77 +19,9 @@ timer = Timer.create();
 detector = Detector.create(settings.detector);
 
 detector.on('start', function(){
-    timer.start();
-    webSocket.write('{"command" : "start-timer"}');
-    console.log('detector start');
-    leds.clearAllLeds();
+    console.log("Detector is gestart");
 })
 
 detector.on('finish', function(){
-    console.log('detector stop');
-    mbed.write('{"stop": ' + settings.pololu.code + '}');
-    var laptime = timer.stop();
-    webSocket.write('{"command": "finish", "time": ' + laptime + '}');
-    leds.clearAllLeds();
-    detector.disable();
+    console.log("Detector is gestopt");
 })
-
-
- /**
- * Leds
- */
-
-var red = { red: 100};
-var orange = { red: 100, green: 100};
-var green = { green: 100};
-
-leds = Leds.create(settings.i2c);
-
-startLeds = function(){
-    setTimeout(function(){
-        leds.setColor(1,red);
-    }, 0);
-
-    setTimeout(function(){
-        leds.setColor(2,orange);
-    }, 1000);
-
-    setTimeout(function(){
-        leds.setColor(3,green);
-        mbed.write('{"start": 1234}');
-        detector.enable();
-    }, 2000);
-}
-
-/**
- * Http server
- */
-
-httpServer = HttpServer.create(settings.http);
-
-
-/**
- * Websocket server
- */
-
-webSocket = WebSocket.create(settings.websocket);
-
-webSocket.on('start', function(){
-    startLeds();
-});
-
-webSocket.on('cancel', function(){
-    mbed.write('{"stop": ' + settings.pololu.code + '}');
-    detector.disable();
-    timer.reset();
-});
-
-webSocket.on("connection", function(){
-    webSocket.write(JSON.stringify({ 'timer-status': timer.getStatus()}));
-});
-
-/**
- *  MBED start/stop robot
- */
-
-var mbed = Mbed.create(settings.mbed);
